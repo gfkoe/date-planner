6f65e8f9f0e96f9ce7ab933cb45153c90b39255d
Gabe Koeb <gfkoeb@ncsu.edu>
2024-12-25 17:06:04 -0500

    Update types

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,1 +1,17 @@
 export type Dino = { name: string; description: string };
+
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: string;
+};
+
+export type Date = {
+  id: number;
+  description: string;
+  location: string;
+  date: Date;
+};
