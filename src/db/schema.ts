d829cd5e18010dfe89bd9b1d42c72792a44e5c7a
Gabe Koeb <gfkoeb@ncsu.edu>
2024-12-25 10:10:58 -0500

    add date schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,15 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, date } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial().primaryKey().notNull(),
   name: text(),
   email: text().unique().notNull(),
   password: text().notNull(),
+});
+
+export const dates = pgTable("dates", {
+  id: serial().primaryKey().notNull(),
+  description: text(),
+  location: text(),
+  date: date().notNull(),
 });
