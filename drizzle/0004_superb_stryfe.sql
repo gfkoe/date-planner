9b9bb8f6db68e3211595addd56b980a2e8103fa7
Gabriel Koeb <112530766+gfkoe@users.noreply.github.com>
2025-01-02 00:08:39 -0500

    Delete drizzle directory

--- drizzle/0004_superb_stryfe.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "dates" ALTER COLUMN "description" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "dates" ALTER COLUMN "location" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "firstName" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "lastName" DROP NOT NULL;
