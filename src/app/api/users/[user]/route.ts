98231ec668f4b29414a4ca81820b2a0800f15967
Gabe Koeb <gfkoeb@ncsu.edu>
2025-01-03 18:34:40 -0500

    Update the api route for user so that we can avoid those vercel errors

--- src/app/api/users/[user]/route.ts
+++ src/app/api/users/[user]/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest } from "next/server";
-import { findUserById } from "../../../../db/db.ts";
+import { findUserById } from "@/db/db";
 
 export const GET = async (
   _request: NextRequest,
   { params }: { params: { user: string } },
 ) => {
-  const awaitedParams = await params;
-  const userId = Number(awaitedParams.user);
+  params = await params;
+  const userId = Number(params.user);
 
   if (!userId) {
     return Response.json({ error: "No user id provided" }, { status: 404 });
