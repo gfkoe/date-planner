db0001ca28a8d75dec8589e53dc93a50a65c2d6e
Gabe Koeb <gfkoeb@ncsu.edu>
2025-01-03 19:33:19 -0500

    remove .ts extension

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm/relations";
-import { users as userSchema } from "./schema.ts";
+import { users as userSchema } from "./schema";
 
 export const usersRelations = relations(userSchema, ({ one, many }) => ({
   friends: many(userSchema),
