5a224fb29ac1bd089305de994ccb12888a61a7e6
Gabe Koeb <gfkoeb@ncsu.edu>
2025-02-18 17:43:34 -0500

    add user search bar

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,17 +1,42 @@
 import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 //import { FaSearch } from "react-icons/fa";
 
 export default function UserSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
   const [searchTerm, setSearchTerm] = useState("");
+
   const [foundUsers, setFoundUsers] = useState([]);
   useEffect(() => {
     //Will need to add fetch for api call here
   }, [searchTerm]);
 
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+    console.log(term);
+  }
+
   return (
     <div>
-      <Input type="text" value={searchTerm} placeholder="Search Users" />
+      <Input
+        type="text"
+        value={searchTerm}
+        placeholder="Search Users"
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
     </div>
   );
 }
